1c8f0c1165ba84a9f6e49614805c491595212f43
williankargo <pinkuanxie@gmail.com>
2021-06-19 22:00:07 +0800

    完善register功能

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -4,7 +4,7 @@
 		<image src="../../static/logo-1.png" class="logo" mode="widthFix"></image> 
 		<view class="loge-title">EMOS企業在線辦公系統</view>
 		<view class="loge-subtitle">Ver 2050.2</view>
-		<!-- pen-type很重要？ -->
+		<!-- open-type很重要？讓下面可以用getUserInfo -->
 		<button class="login-btn" open-type="getUserInfo" @tap="login()" >登入系統</button>
 		<view class="register-container">
 			沒有帳號？
@@ -21,7 +21,11 @@
 			}
 		},
 		methods: {
-			
+			toRegister: function(){
+				uni.navigateTo({
+					url:'../register/register'
+				});
+			}
 		}
 	}
 </script>
--- pages/register/register.vue
+++ pages/register/register.vue
@@ -1,12 +1,12 @@
 <template>
 	<view>
-		<input type="image" src="../../static/logo-2.png" mode="widthFix" class="logo"/>
+		<image src="../../static/logo-2.png" mode="widthFix" class="logo"/>
 		<view class="register-container">
 			<input type="text" placeholder="輸入你的邀請碼" class="register-code"
-			 maxlength="6" v-model="register-code" />
+			 maxlength="6" v-model="registerCode" />
 			 <view class="register-desc">
 			 	管理員創建員工證帳號後，你可以從你的個人郵箱中獲取註冊邀請碼
-				<button class="register-btn" open-type="getUserInfo" @tap="register()"></button>
+				<button class="register-btn" open-type="getUserInfo" @tap="register()">執行註冊</button>
 			 </view>
 		</view>
 	</view>
@@ -16,11 +16,28 @@
 	export default {
 		data() {
 			return {
-				
+				registerCode: null
 			}
 		},
 		methods: {
-			
+			register:function(){ // 得到臨時授權字符串
+				uni.login({
+					provider: "weixin",
+					success: function(resp){
+						//console.log(resp.code)
+						let code = resp.code;
+						uni.getUserProfile({
+							desc: '獲取用戶信息',
+							success: function(resp){
+								let nickName = resp.userInfo.nickName;
+								let avatarUrl = resp.userInfo.avatarUrl; // 頭像
+								console.log(nickName)
+								console.log(avatarUrl)
+							}
+						})
+					}
+				})
+			}
 		}
 	}
 </script>
